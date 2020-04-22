af84ae4235ca3640720923117c7f9e07888b0e0a
lisaau <lisaau@users.noreply.github.com>
2020-04-21 21:33:31 -0700

    Add PUT /plant

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); // needed for Postman
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
@@ -43,6 +43,31 @@ app.delete('/plant', (req, res) => {
         res.json(plants.splice(index, 1));
     }
     // res.json(plants.filter(plant => plant.id !== parseInt(plantId)))
+});
+
+// returns array with first element as modified plant and second element the index in the plants array
+// use the plant object and index to re-render UI
+app.put('/plant', (req, res) => {
+    let { plantId, plantName, plantSpecies } = req.body;
+    plantId = parseInt(plantId);
+    // take ID, modify plant with that id
+    plants.forEach(plant => {
+        if (plant.id === plantId) {
+            plant.name = plantName;
+            plant.species = plantSpecies;
+        }
+    })
+        
+    // return that modified plant object with plants.find(p => p.id === plantId) and its corresponding index in plants array
+    let editedPlant = plants.find(p => p.id === plantId);
+    console.log(editedPlant)
+    let editedPlantIndex;
+    for(let i = 0; i < plants.length; i += 1) {
+        if(plants[i]['id'] === plantId) {
+            editedPlantIndex = i;
+        }
+    }
+    res.json([editedPlant, editedPlantIndex]) 
 });
 
 const PORT = process.env.PORT || 8000;
