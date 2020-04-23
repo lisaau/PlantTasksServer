c3912fc97ae854f8188ad2712fa53296f3d83e78
lisaau <lisaau@users.noreply.github.com>
2020-04-22 17:36:16 -0700

    Set up DB and DB calls

---
+++ planttasks.sql
@@ -0,0 +1,98 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.2
+-- Dumped by pg_dump version 12.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: plants; Type: TABLE; Schema: public; Owner: tpl619_2
+--
+
+CREATE TABLE public.plants (
+    id integer NOT NULL,
+    name text NOT NULL,
+    species text NOT NULL,
+    notes text,
+    image text
+);
+
+
+ALTER TABLE public.plants OWNER TO tpl619_2;
+
+--
+-- Name: plants_id_seq; Type: SEQUENCE; Schema: public; Owner: tpl619_2
+--
+
+CREATE SEQUENCE public.plants_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.plants_id_seq OWNER TO tpl619_2;
+
+--
+-- Name: plants_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tpl619_2
+--
+
+ALTER SEQUENCE public.plants_id_seq OWNED BY public.plants.id;
+
+
+--
+-- Name: plants id; Type: DEFAULT; Schema: public; Owner: tpl619_2
+--
+
+ALTER TABLE ONLY public.plants ALTER COLUMN id SET DEFAULT nextval('public.plants_id_seq'::regclass);
+
+
+--
+-- Data for Name: plants; Type: TABLE DATA; Schema: public; Owner: tpl619_2
+--
+
+COPY public.plants (id, name, species, notes, image) FROM stdin;
+1	Planty	Air Plant	Taking care of Planty for Kim	\N
+2	Planty 2	Green Onion	Tastes delicious	\N
+3	Planty 3	Iris	\N	\N
+5	Planty 4	Tulip	\N	\N
+4	Daisy the Daisy	Daisy	\N	\N
+\.
+
+
+--
+-- Name: plants_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tpl619_2
+--
+
+SELECT pg_catalog.setval('public.plants_id_seq', 6, true);
+
+
+--
+-- Name: plants plants_pkey; Type: CONSTRAINT; Schema: public; Owner: tpl619_2
+--
+
+ALTER TABLE ONLY public.plants
+    ADD CONSTRAINT plants_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ src/plantTasks-database.js
@@ -0,0 +1,53 @@
+const pgp = require('pg-promise')();
+
+/**
+ * An object that has methods matching useful database queries.
+ * Use `this.db` to access a connected pg-promise connection.
+ * Make sure to return the promise!
+ *
+ * For examples of other queries, see
+ * [pghttps://github.com/vitaly-t/pg-promise/wiki/Learn-by-Example
+ */
+class PlantTasksDatabase {
+  /**
+   * @param {String} name - name of database to connect to
+   */
+  constructor(name) {
+    const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`;
+    console.log('Postgres DB => ', connectionString);
+    this.db = pgp(connectionString);
+  }
+
+  sanityCheck() {
+    console.log('\tTesting database connection...');
+    return this.getPlantsCount().then(count =>
+      console.log(`\t✔️ Found ${count} plants.`)
+    );
+  }
+
+  getPlantsCount() {
+    return this.db.one('SELECT count(*) FROM plants').then(r => r.count);
+  }
+
+  getAllPlants() {
+    return this.db.any('SELECT * FROM plants');
+  }
+
+  getPlant(plantId) {
+    return this.db.one('SELECT * FROM plants WHERE id = $1', plantId);
+  }
+
+  addPlant(plantName, plantSpecies) {
+    return this.db.one('INSERT INTO plants (name, species) VALUES($1, $2) RETURNING *',[plantName, plantSpecies]);
+  }
+  
+  deletePlant(plantId) {
+    return this.db.one('DELETE FROM plants WHERE id = $1 RETURNING *', plantId);
+  }
+  
+  editPlant(plantId, plantName, plantSpecies) {
+    return this.db.one('UPDATE plants SET name = $2, species = $3 WHERE id = $1 RETURNING *', [plantId, plantName, plantSpecies]);
+  }
+}
+
+module.exports = PlantTasksDatabase;
