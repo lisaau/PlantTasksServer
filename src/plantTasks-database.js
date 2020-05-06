f09112a34851c3a7f484d978fccd6d8d9918ad87
lisaau <lisaau@users.noreply.github.com>
2020-05-06 12:41:50 -0700

    Convert user's current time to UTC to match task instance date in DB

--- src/plantTasks-database.js
+++ src/plantTasks-database.js
@@ -88,13 +88,12 @@ class PlantTasksDatabase {
       WHERE p.user_id = $1`, userId)
   }
 
-  // convert UTC time to PST. temporary solution; will pass user's timezone later
   getTodayTaskInstances(userId) {
     return this.db.any(`SELECT ti.id AS task_instance_id, ti.completed, ti.due_date, t.id AS task_id, t.description, t.frequency, p.id AS plant_id, p.name
     FROM task_instances AS ti
     INNER JOIN tasks AS t ON t.id = ti.task_id
     INNER JOIN plants AS p on p.id = t.plant_id
-    WHERE date_trunc('day', ti.due_date) = date_trunc('day', now() AT TIME ZONE 'PST') AND p.user_id = $1`, userId)
+    WHERE date_trunc('day', ti.due_date) = date_trunc('day', now() AT TIME ZONE 'UTC') AND p.user_id = $1`, userId)
   }
   
   getTaskInstancesByDay(date, userId) {
